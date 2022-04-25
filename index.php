0eb90325b7e2f4221246ad51ec5f0a3bc5e0d46e
renaldiangg <103799681+renaldiangg@users.noreply.github.com>
2022-04-25 10:13:31 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,6 @@
 <?php
 
-echo "hello kakak ahyar ganteng kan ?? ";
+echo ("hello kakak ahyar ganteng kan ?? "\n);
+echo (" Nama : Renaldi Anggarkusuma \n");
+echo (" NIM : 52019028 \n");
+echo (" Jurusan Teknik Informatika \n");
